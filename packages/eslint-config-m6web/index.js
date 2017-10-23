e1080964e81c6601029e0315c44b1a6eadef2646
f-pensato <flavien.pensato@gmail.com>
2017-10-23 11:48:56 +0200

    chore(lerna): try a new structure for the config M6 web

---
+++ packages/eslint-config-m6web/index.js
@@ -0,0 +1,73 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier'],
+  env: {
+    node: true,
+  },
+  rules: {
+    // use "_this" as a  variable to pass "this" instance if it's needed
+    'consistent-this': ['error', '_this'],
+
+    // don't allow "if" on a single line
+    curly: ['error', 'all'],
+
+    // function name are required, this is not just a warning
+    'func-names': 'error',
+
+    // line length
+    'max-len': [
+      'error',
+      120,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+
+    // new line before return statement is required
+    'newline-before-return': 'error',
+
+    // console statements don't warn, this is forbidden
+    'no-console': 'error',
+
+    // inline comments are forbidden
+    'no-inline-comments': 'error',
+
+    // prefer destructuring on object declaration only
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+
+    // allow ".js" to be a valid extension for JSX files
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+
+    // TODO: remove these rules when next version of Airbnb config will be released
+    'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
+    'react/no-unused-prop-types': ['error', { skipShapeProps: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 120,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
